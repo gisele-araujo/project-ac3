c54baa6a8cf0d0e1b6d1b52650a45088bb17d753
gisele-araujo <gisele.flor@bandtec.com.br>
2021-05-19 21:55:45 -0300

    add projeto

---
+++ locadora/src/test/java/br/com/bandtec/locadora/LocadoraApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bandtec.locadora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocadoraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
